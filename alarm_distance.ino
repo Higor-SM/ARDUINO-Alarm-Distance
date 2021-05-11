bc68c7d1496b4f03949b965d2b53d540aa84b039
Higor-SM <72944799+Higor-SM@users.noreply.github.com>
2021-05-11 14:28:56 -0300

    Add files via upload

---
+++ alarm_distance.ino
@@ -0,0 +1,50 @@
+#define trigPin 11 
+#define echoPin 12 
+#define period 10 
+
+int freq= 0; 
+int buzz= 13;
+int dur, dist;
+ 
+void setup() 
+{
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(buzz, OUTPUT);
+}
+ 
+void loop() 
+{
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPin, LOW);
+ dur= pulseIn(echoPin, HIGH);
+ dist= (dur / 2) / 29.1; 
+ 
+ if (dist>= 40 || dist<= 0) 
+ {
+ Serial.println("target");
+ digitalWrite(buzz, LOW);
+ }
+ 
+ else 
+  {
+   Serial.println("unknown detected");
+   Serial.print("dist= ");
+   Serial.print(dist); 
+ 
+   for (freq= 150; freq < 1800; freq+= 1) 
+    { 
+    tone(buzz, freq, period);
+    delay(3);
+    }
+ 
+   for (freq= 1800; freq > 150; freq-= 1) 
+    { 
+    tone(buzz, freq, period);
+    delay(3);
+    }
+ 
+  }
+}
